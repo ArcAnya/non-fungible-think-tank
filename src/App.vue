44223e6ff2ce3f9180f0c5fdf128c43ee55ac21d
Rain Prääts <rain_w_p@hotmail.com>
2022-03-01 08:45:22 +0100

    Count Button

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <!-- <img alt="NFTT logo" src="./assets/LOL.jpg"> -->
   <HelloWorld msg="Welcome to NFTT"/>
+  <button @click="count++">Count is: {{ count }}</button>
 </template>
 
 <script>
@@ -10,17 +11,27 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+
+  data() {
+    return {
+      count: 0
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font: 1em/150% Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+button {
+  font-size: 2em;
 }
 </style>
