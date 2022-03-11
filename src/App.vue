8273b862747026bf1c62e1428ac8cdf9cb95de45
Rain Prääts <rain_w_p@hotmail.com>
2022-03-11 17:09:46 +0100

    Replaced all files

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,76 @@
-<template>
-  <img alt="NFTT logo" src="./assets/LOL.jpg">
-  <HelloWorld msg="Welcome to NFTT"/>
-  <button @click="count++">Count is: {{ count }}</button>
-</template>
+<script setup>
+// App for a Count button using Composition Api
+import { ref, onMounted } from 'vue'
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+// reactive state
+const count = ref(0)
+let walletConnected = ref(false)
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
+// functions that mutate state and trigger updates
+function increment() {
+  count.value++
+}
 
-  data() {
-    return {
-      count: 0
-    }
+function loginToggle() {
+  if (walletConnected.value === true) {
+    walletConnected.value = false
+  } else {
+    walletConnected.value = true
   }
 }
+
+// lifecycle hooks
+onMounted(() => {
+  console.log(`The initial count is ${count.value}.`)
+})
+
 </script>
 
-<style>
-#app {
-  font: 1em/150% Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<template>
+  <header>
+    <a href="#validators" title="Read about the validators.">Validators</a>
+    <a href="#Discord" title="Join our discord.">Discord</a>
+    <button @click="loginToggle" v-if="walletConnected">Log out</button>
+    <button @click="loginToggle" v-else>Connect Wallet</button>
+  </header>
+  <main>
+    <h1>NFTT</h1>
+    <p>Non fungable think tank</p>
+  <button class="mainlogin">Connect Wallet</button>
+  </main>
+</template>
+
+<style scoped>
+
+/* Global Styling */
+
+/* Header */
+
+header {
+  height: 8vh;
+  border-bottom: 2px solid black;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 35%;
+  position: relative;
 }
 
-button {
-  font-size: 2em;
+header button {
+  padding: 1em 2em;
+  width: 150px;
+  position: absolute;
+  right: 1%;
+}
+
+/* main */
+
+main {
+  flex: 1;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
 }
 </style>
